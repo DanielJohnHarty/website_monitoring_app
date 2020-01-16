d6ff03d0fd0873b5a3f3b10d0de483890ab5c8cd
Daniel Harty <dan.john.harty@gmail.com>
2020-01-16 23:30:19 +0100

    Initial commit - skeleton Website class and async process

---
+++ website.py
@@ -0,0 +1,85 @@
+import httpx
+import datetime
+
+# import aiohttp
+# from stat import Stat
+
+
+class Website:
+    def __init__(self, url=None, check_interval=None):
+        self.url = url
+        self.check_interval = check_interval
+        # self.stats = Stat()
+
+    def get_next_ping_time(self, dt: datetime = None) -> datetime:
+        """
+        Increments the time of the reference datetime parameter by
+        the self.check_interval value in seconds.
+
+        If no reference datetime value is passed, it returns the current
+        datetime.
+
+        PARAMETERS:
+            dt: A datetime object
+        RETURNS: A datetime object
+        """
+        if dt:
+            next_ping_at = dt + datetime.timedelta(seconds=self.check_interval)
+        else:
+            next_ping_at = datetime.datetime.now()
+
+        return next_ping_at
+
+    def generate_update(period: int) -> str:
+        """
+        Generates a string describing the stats
+        of the Website instance over the period
+        passed as a parameter e.g. over 10 minutes
+        or over 2 minutes
+
+        PARAMETERS:
+            period: In minutes as int
+
+        RETURNS: str
+        """
+        return "Placeholder result string"
+
+    async def monitor_website(self) -> None:
+        """
+        Async loop sending requests to self.url
+        at the freauency of the self.check_interval.
+
+        It is in charge of keeping self.stats up to date.
+
+        PARAMETERS: None
+        RETURNS: None
+        """
+
+        # async with aiohttp.ClientSession() as session:
+        async with httpx.AsyncClient() as client:
+
+            next_ping_time = self.get_next_ping_time()
+
+            while True:
+                if next_ping_time <= datetime.datetime.now():
+                    # asyncio.sleep(self.check_interval)
+                    
+                    r = await self.ping_url(client, self.url)
+                    
+                    print(f"Pinged {self.url}:{r} in {str(r.elapsed)}.")
+                    next_ping_time = self.get_next_ping_time(next_ping_time)
+
+    async def ping_url(self, client, url) -> None:
+        """
+        Responsible for making a single reauest to
+        self.url and updating self.stats with the
+        results
+
+        PARAMETERS: None
+        RETURNS: None
+        """
+        # async with client.get(url) as response:
+        #   return await response
+
+        return await client.get(url, timeout=None)
+
---
+++ website_monitoring_app.py
@@ -0,0 +1,64 @@
+import asyncio
+from website import Website
+
+LINE_LENGTH = 60
+BOLD_LINE = "=" * LINE_LENGTH
+SINGLE_LINE = "-" * LINE_LENGTH
+WELCOME_MSG = "Website monitoring application"
+GOODBYE_MSG = "Come back soon!"
+
+
+def get_websites_to_monitor():
+    """
+    Enables user to input website data which is
+    used to create Website instances as part of a
+    list.
+
+    PARAMETERS: None
+    RETURNS: list
+    """
+    websites = []
+    while True:
+        print("Please enter the a website you wish to monitor")
+        url = input().lower()
+        print("How many seconds between consequetive checks?:")
+        check_interval = int(input())
+        website = Website(url=url, check_interval=check_interval)
+        websites.append(website)
+        print(f"Your website has been added to the monitoring list.")
+        print(f"In total that makes {len(websites)}. Add another? [y/n]")
+
+        if input().lower() in ["yes", "y"]:
+            continue
+        else:
+            break
+
+    return websites
+
+
+async def monitor_websites(websites_to_monitor):
+
+    coros = [
+        asyncio.create_task(website.monitor_website())
+        for website in websites_to_monitor
+    ]
+
+    print(f"Beginning website monitoring...")
+
+    await asyncio.gather(*coros)
+
+
+if __name__ == "__main__":
+
+    print(f"{BOLD_LINE}\n{WELCOME_MSG}\n{SINGLE_LINE}")
+
+    websites_to_monitor = get_websites_to_monitor()
+
+    asyncio.run(monitor_websites(websites_to_monitor))
+
+
+"""
+http://google.com
+http://ign.com
+http://office.com
+"""
